419878fb217c8d87abe87fa1eb74369d2b66c5f7
Kristoffer <481270+kristofferR@users.noreply.github.com>
2026-04-05 04:50:50 +0200

    Update IPTV Checker asset URLs for v1.0.0

--- Formula/iptv-checker.rb
+++ Formula/iptv-checker.rb
@@ -6,22 +6,22 @@ class IptvChecker < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_aarch64.app.tar.gz"
+      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_mac_arm.app.tar.gz"
       sha256 "f4a03acfeeb743787677338d774c324e7325411acea7b64f48df2cb83552490d"
     end
     on_intel do
-      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_x64.app.tar.gz"
+      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_mac_x64.app.tar.gz"
       sha256 "4c93dcd2e6f454576c91119085d34343095f8f6c412f61973e130bce38486184"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_aarch64.AppImage"
+      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_lin_arm.AppImage"
       sha256 "304ff7be0f611e4ea801d99a4876ab8e9f7e6c00490b0a1ea52445700b942445"
     end
     on_intel do
-      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_amd64.AppImage"
+      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_lin_x64.AppImage"
       sha256 "ed6c145832d7d409ec4be34c7c37e58a70a29cedf08f737dd858db8cc0e4ca5d"
     end
   end
