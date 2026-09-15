954ee0aeb992795f504ab46d505897c20e93b4c4
kristofferR <481270+kristofferR@users.noreply.github.com>
2026-09-16 01:58:26 +0200

    fix: update IPTV Checker cask to supported Homebrew syntax

--- Casks/iptv-checker.rb
+++ Casks/iptv-checker.rb
@@ -5,15 +5,13 @@ cask "iptv-checker" do
     on_arm do
       sha256 "f1aaf50325960df301554a5ca13db82346b8a596dbf0984178f41c5066ed7cb9"
 
-      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_mac_arm.dmg",
-verified: "github.com/kristofferR/IPTVChecker/"
+      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_mac_arm.dmg"
     end
 
     on_intel do
       sha256 "0d797edd9339157ff1f6c3296f632480734c54f6cc06a85f80f7cf024bbef773"
 
-      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_mac_x64.dmg",
-verified: "github.com/kristofferR/IPTVChecker/"
+      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_mac_x64.dmg"
     end
   end
 
@@ -21,15 +19,13 @@ verified: "github.com/kristofferR/IPTVChecker/"
     on_arm do
       sha256 "7258bbaddf0f4eebf03044dd74c3b222d930098b3345866b1fa4c84fc8f438ff"
 
-      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_lin_arm.AppImage",
-verified: "github.com/kristofferR/IPTVChecker/"
+      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_lin_arm.AppImage"
     end
 
     on_intel do
       sha256 "3f4a63c53b49de9b3ab42b4d6f853a34eae3f3edb619a0549494129504775085"
 
-      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_lin_x64.AppImage",
-verified: "github.com/kristofferR/IPTVChecker/"
+      url "https://github.com/kristofferR/IPTVChecker/releases/download/v#{version}/IPTV.Checker_#{version}_lin_x64.AppImage"
     end
 
     container type: :naked
@@ -43,12 +39,11 @@ verified: "github.com/kristofferR/IPTVChecker/"
   # update itself between `brew upgrade` runs.
   auto_updates true
 
-  preflight do
-    old_app = Pathname.new("/Applications/IPTV Checker.app")
-    next unless old_app.symlink?
-
-    target = old_app.readlink.to_s
-    old_app.delete if target.include?("/opt/homebrew/opt/iptv-checker/") || target.include?("/usr/local/opt/iptv-checker/")
+  preflight_steps do
+    on_macos do
+      remove "/Applications/IPTV Checker.app", symlink_target_contains: "/opt/homebrew/opt/iptv-checker/"
+      remove "/Applications/IPTV Checker.app", symlink_target_contains: "/usr/local/opt/iptv-checker/"
+    end
   end
 
   on_macos do
